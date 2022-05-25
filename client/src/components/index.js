5c6c3c0031d6636a51399091a623973bb4de3c92
CristianCeccon <cristian_ceccon45@hotmail.com>
2022-05-24 21:28:12 -0300

    Criação base dos arquivos .jsx + Index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { defafult as Loader } from './Loader';
+export { defafult as Footer } from './Footer';
+export { defafult as Navbar } from './Navbar';
+export { defafult as Welcome } from './Welcome';
+export { defafult as Services } from './Services';
+export { defafult as Transactions} from './Transactions';
